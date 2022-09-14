4b9a28d88b974f2369225654edcfc21c17150af8
leoaugustov <vieira.augustoleonardo@gmail.com>
2022-09-14 17:14:49 -0300

    Typo fix

--- test/file-to-queue.test.js
+++ test/file-to-queue.test.js
@@ -9,7 +9,7 @@ jest.mock('../src/sqs', () => ({
     sendMessage: jest.fn()
 }));
 
-it('should consume file lines and send a message with each one them', async () => {
+it('should consume file lines and send a message with each one of them', async () => {
     const file = 'path/filename.csv';
     const queueUrl = 'https://sqs.us-east-1.amazonaws.com/00000000/test-queue';
 
@@ -18,8 +18,8 @@ it('should consume file lines and send a message with each one them', async () =
     expect(consumeLines.mock.calls.length).toBe(1);
     expect(consumeLines.mock.calls[0][0]).toEqual(file);
 
-    const message = 'some message';
-    await consumeLines.mock.calls[0][1](message);
+    const line = 'some message';
+    await consumeLines.mock.calls[0][1](line);
 
-    expect(sendMessage).toBeCalledWith(expect.any(SQSClient), queueUrl, message);
+    expect(sendMessage).toBeCalledWith(expect.any(SQSClient), queueUrl, line);
 });
