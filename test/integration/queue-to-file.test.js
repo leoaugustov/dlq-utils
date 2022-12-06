2053fba4317c91bde3ca9b5866ffb87f61d1a534
leoaugustov <vieira.augustoleonardo@gmail.com>
2022-12-06 18:39:37 -0300

    Fix test

--- test/integration/queue-to-file.test.js
+++ test/integration/queue-to-file.test.js
@@ -25,7 +25,6 @@ afterAll(async () => {
 it('should consume messages from queue and save them in file', async () => {
   const queueUrl = getQueueUrl(QUEUE_NAME);
   const fileName = temporaryFile();
-  const messages = ['message-1', 'message-2', 'message-3', 'message-4'];
 
   await sendMessage(sqsClient, QUEUE_NAME, 'message-1');
   await sendMessage(sqsClient, QUEUE_NAME, 'message-2');
